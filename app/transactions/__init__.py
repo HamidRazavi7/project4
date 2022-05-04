5e80a0f92bcd431f2b1bbcf1b6211021b19b9cde
Hamid Razavi <razavi.hamid85@gmail.com>
2022-05-04 22:34:06 +0800

    created transaction database.

--- app/transactions/__init__.py
+++ app/transactions/__init__.py
@@ -7,48 +7,48 @@ from flask_login import current_user, login_required
 from jinja2 import TemplateNotFound
 
 from app.db import db
-from app.db.models import Song
-from app.songs.forms import csv_upload
+from app.db.models import Transaction
+from app.transactions.forms import csv_upload
 from werkzeug.utils import secure_filename, redirect
 
-songs = Blueprint('songs', __name__,
+transactions = Blueprint('transactions', __name__,
                   template_folder='templates')
 
 
-@songs.route('/songs', methods=['GET'], defaults={"page": 1})
-@songs.route('/songs/<int:page>', methods=['GET'])
-def songs_browse(page):
+@transactions.route('/transactions', methods=['GET'], defaults={"page": 1})
+@transactions.route('/transactions/<int:page>', methods=['GET'])
+def transactions_browse(page):
     page = page
     per_page = 1000
-    pagination = Song.query.paginate(page, per_page, error_out=False)
+    pagination = Transaction.query.paginate(page, per_page, error_out=False)
     data = pagination.items
     try:
-        return render_template('browse_songs.html', data=data, pagination=pagination)
+        return render_template('browse_transactions.html', data=data, pagination=pagination)
     except TemplateNotFound:
         abort(404)
 
 
-@songs.route('/songs/upload', methods=['POST', 'GET'])
+@transactions.route('/transactions/upload', methods=['POST', 'GET'])
 @login_required
-def songs_upload():
+def transactions_upload():
     form = csv_upload()
     log = logging.getLogger("csvupload")
     if form.validate_on_submit():
         filename = secure_filename(form.file.data.filename)
         filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
         form.file.data.save(filepath)
-        # user = current_user
-        list_of_songs = []
+        list_of_transactions = []
         with open(filepath) as file:
             csv_file = csv.DictReader(file)
             for row in csv_file:
-                list_of_songs.append(Song(row['Title'], row['Artist'], row['Genre'], row['Year']))
+                print (row)
+                list_of_transactions.append(Transaction(row['AMOUNT'], row['TYPE'],))
 
-        current_user.songs = list_of_songs
+        current_user.transactions = list_of_transactions
         db.session.commit()
 
         log.info('Music CSV Was Uploaded')
-        return redirect(url_for('songs.songs_browse'))
+        return redirect(url_for('transactions.transactions_browse'))
 
     try:
 
