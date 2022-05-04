e5fb699a045103c0fe3fe67b70f18229dd6d5f30
Hamid Razavi <razavi.hamid85@gmail.com>
2022-05-04 13:13:17 +0800

    Unit test to check the calculator's add method.

---
+++ tests/operations_test.py
@@ -0,0 +1,14 @@
+"""Testing the Calculator"""
+# From specifies the namespace
+from calculator import Calculator
+
+
+def test_calculator_add_method():
+    """Testing the Calculator add"""
+    calculator = Calculator()
+    # this is show using the calculator object add method
+    assert calculator.add(1, 1) == 2
+
+def test_my_first_test_add():
+    """Testing the simplest addition"""
+    assert 1 + 1 == 2
