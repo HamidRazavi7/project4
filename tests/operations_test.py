ccb4f77048d95f723321f70f6f98b4d1a2d21958
Hamid Razavi <razavi.hamid85@gmail.com>
2022-05-04 13:17:30 +0800

    Unit test to check the calculator's multiply method.

--- tests/operations_test.py
+++ tests/operations_test.py
@@ -9,15 +9,12 @@ def test_calculator_add_method():
     # this is show using the calculator object add method
     assert calculator.add(1, 1) == 2
 
-def test_my_first_test_add():
-    """Testing the simplest addition"""
-    assert 1 + 1 == 2
-
 def test_calculator_subtract_method():
     """Testing the Calculator Subtract"""
     calculator = Calculator()
     assert calculator.subtract(1, 1) == 0
 
-def test_my_first_test_subtract():
-    """Testing the simplest subtraction"""
-    assert 1 - 1 == 0
+def test_calculator_multiply_method():
+    """Testing the Calculator multiply"""
+    calculator = Calculator()
+    assert calculator.multiply(1, 1) == 1
