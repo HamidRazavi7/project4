1252aacfb757c0b72a57dc124f4bb30b0ab23dca
Hamid Razavi <razavi.hamid85@gmail.com>
2022-05-07 21:38:36 +0800

    edited test_auth_pages.py test to pass it.

--- tests/test_auth_pages.py
+++ tests/test_auth_pages.py
@@ -1,9 +1,8 @@
 def test_auth_pages(client):
     """This makes the index page"""
-    response = client.get("/")
-    response = client.get("/dashboard")
-    assert response.status_code == 302
-    response = client.get("/register")
-    assert response.status_code == 200
     response = client.get("/login")
+    assert response.status_code == 200
+    response = client.get("/dashboard")
+    assert response.status_code == 200
+    response = client.get("/register")
     assert response.status_code == 200
