6bd250d5a3506ff517d50031fe7867330741a970
Hamid Razavi <razavi.hamid85@gmail.com>
2022-05-07 22:17:51 +0800

    edited the registration page test.

--- tests/test_auth_pages.py
+++ tests/test_auth_pages.py
@@ -2,7 +2,12 @@ def test_auth_pages(client):
     """This makes the index page"""
     response = client.get("/login")
     assert response.status_code == 200
-#    response = client.get("/dashboard")
-#    assert response.status_code == 200
+    # POST to the login with the email and password to authenticate
+    response = client.post("/login", data = {
+        "email": "test@test",
+        "password": "testtest"
+    })
+    #response = client.get("/dashboard")
+    assert response.status_code == 302
     response = client.get("/register")
-    assert response.status_code == 200
+    assert response.status_code == 302
