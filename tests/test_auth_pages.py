9a16e96403e161f7073038a0486263f42f153914
Hamid Razavi <razavi.hamid85@gmail.com>
2022-05-07 21:41:43 +0800

    added index page assert to test_auth_pages.py

--- tests/test_auth_pages.py
+++ tests/test_auth_pages.py
@@ -1,5 +1,9 @@
 def test_auth_pages(client):
     """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/login"' in response.data
+    assert b'href="/register"' in response.data
     response = client.get("/login")
     assert response.status_code == 200
     response = client.get("/dashboard")
